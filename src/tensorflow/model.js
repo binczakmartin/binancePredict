42e1a0f850741fdf8d7617733d7c42737a834929
Martin Binczak <martin@pragmea.fr>
2023-03-05 20:54:52 +0100

    improve neural network

--- src/tensorflow/model.js
+++ src/tensorflow/model.js
@@ -1,5 +1,5 @@
 import fs from "fs";
-import * as tf from '@tensorflow/tfjs-node';
+import * as tf from "@tensorflow/tfjs-node";
 import { savedModelPath } from "../constants.js";
 import { resolve } from "path";
 
@@ -7,8 +7,10 @@ export const loadModel = async function (pair) {
   let model;
 
   if (fs.existsSync(`${savedModelPath}/${pair}/model.json`)) {
-    model = await tf.loadLayersModel(`file://${resolve(savedModelPath)}/${pair}/model.json`);
-    model.compile({ loss: "meanSquaredLogarithmicError", optimizer: 'adam' });
+    model = await tf.loadLayersModel(
+      `file://${resolve(savedModelPath)}/${pair}/model.json`
+    );
+    model.compile({ loss: "meanSquaredLogarithmicError", optimizer: "adam" });
     return model;
   }
 
@@ -17,33 +19,44 @@ export const loadModel = async function (pair) {
   const input = tf.input({ shape: [5] });
   const dense1 = tf.layers.dense({
     units: 128,
-    kernelRegularizer: tf.regularizers.l2({ l2: 0.01 })
+    kernelInitializer: "glorotUniform",
+    kernelRegularizer: tf.regularizers.l2({ l2: 0.01 }),
   }).apply(input);
   const activation1 = tf.layers.leakyReLU({ alpha: leakyAlpha }).apply(dense1);
   const dropout1 = tf.layers.dropout({ rate: 0.5 }).apply(activation1);
   const dense2 = tf.layers.dense({
     units: 64,
-    activation: 'relu',
-    kernelRegularizer: tf.regularizers.l2({ l2: 0.01 })
+    kernelInitializer: "glorotUniform",
+    activation: "relu",
+    kernelRegularizer: tf.regularizers.l2({ l2: 0.01 }),
   }).apply(dropout1);
   const dropout2 = tf.layers.dropout({ rate: 0.4 }).apply(dense2);
   const dense3 = tf.layers.dense({
     units: 32,
-    activation: 'relu',
-    kernelRegularizer: tf.regularizers.l2({ l2: 0.01 })
+    kernelInitializer: "glorotUniform",
+    activation: "relu",
+    kernelRegularizer: tf.regularizers.l2({ l2: 0.01 }),
   }).apply(dropout2);
   const dropout3 = tf.layers.dropout({ rate: 0.3 }).apply(dense3);
   const dense4 = tf.layers.dense({
     units: 16,
-    activation: 'relu',
-    kernelRegularizer: tf.regularizers.l2({ l2: 0.01 })
+    kernelInitializer: "glorotUniform",
+    activation: "relu",
+    kernelRegularizer: tf.regularizers.l2({ l2: 0.01 }),
   }).apply(dropout3);
   const dropout4 = tf.layers.dropout({ rate: 0.2 }).apply(dense4);
+  const dense5 = tf.layers.dense({
+    units: 8,
+    kernelInitializer: "glorotUniform",
+    activation: "relu",
+    kernelRegularizer: tf.regularizers.l2({ l2: 0.01 }),
+  }).apply(dropout4);
+  const dropout5 = tf.layers.dropout({ rate: 0.2 }).apply(dense5);
   const output = tf.layers.dense({
     units: 1,
-    activation: 'sigmoid',
-    kernelRegularizer: tf.regularizers.l2({ l2: 0.01 })
-  }).apply(dropout4);
+    activation: "sigmoid",
+    kernelRegularizer: tf.regularizers.l2({ l2: 0.01 }),
+  }).apply(dropout5);
 
   model = tf.model({ inputs: input, outputs: output });
   model.compile({ loss: "binaryCrossentropy", optimizer: tf.train.adam() });
